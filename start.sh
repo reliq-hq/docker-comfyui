88daee711991ae4e04c54e953272a8c33731ed21
Reliq Dev <dev@reliq.uk>
2025-07-23 09:40:41 +0100

    Allow to persist custom_nodes in a docker volume

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-VENV_PATH="/app/venv"
-LAUNCH_COMMAND="python3 /app/main.py --listen"
+APP_DIR="/app"
+CUSTOM_NODES_DIR="$APP_DIR/custom_nodes"
+VENV_PATH="$APP_DIR/venv"
+LAUNCH_COMMAND="python3 $APP_DIR/main.py --listen"
 
 if test "$FORCE_CPU" = "1"; then
     LAUNCH_COMMAND="${LAUNCH_COMMAND} --cpu"
+fi
+
+if test -n "$(find "$CUSTOM_NODES_DIR/" -maxdepth 0 -empty)" ; then
+    echo "$CUSTOM_NODES_DIR is empty: unpack pre-installed set"
+    su comfy -c "tar -xvf \"$APP_DIR/custom_nodes_backup.tar.gz\" -C \"$CUSTOM_NODES_DIR/\""
 fi
 
 export PATH="$VENV_PATH:$PATH"
